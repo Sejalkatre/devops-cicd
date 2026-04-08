c0449620743c67f4e2c586185253d9f04bffc411
Sejal Katre <96768363+Sejalkatre@users.noreply.github.com>
2026-04-08 19:14:45 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,8 +23,8 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.4"   # pin stable version
 
-  name     = "devops-cluster"   # replaces cluster_name
-  version  = "1.29"             # replaces cluster_version
+  name            = "devops-cluster"   # replaces cluster_name
+  cluster_version = "1.29"             # replaces cluster_version
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets   # replaces subnets
