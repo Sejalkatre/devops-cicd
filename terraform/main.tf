21faadf53ba6432c356d5b97bd6b50a8a77e2e59
Sejal Katre <96768363+Sejalkatre@users.noreply.github.com>
2026-04-08 19:22:36 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"   # make sure you use AWS provider 5.x
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
