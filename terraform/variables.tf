f12c2cedf21c95226f2b816914d9260434a7e70b
Sejal Katre <96768363+Sejalkatre@users.noreply.github.com>
2026-04-08 20:23:44 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,14 @@
-variable "region" { default = "us-west-2" }
-variable "ami_id" { default = "ami-0d76b909de1a0595d" }
-variable "key_name" { description = "devops-key" }
+variable "region" {
+  default     = "us-west-2"
+  description = "AWS region to deploy resources"
+}
+
+variable "ami_id" {
+  default     = "ami-0d76b909de1a0595d"
+  description = "AMI ID for Jenkins EC2"
+}
+
+variable "key_name" {
+  default     = "devops-key"   # ✅ this must match the key pair name in AWS
+  description = "Name of the existing AWS key pair"
+}
