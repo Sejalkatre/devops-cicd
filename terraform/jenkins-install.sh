84537d5fb0e57f3f3b59d97b734400b018cabb1b
Sejal Katre <96768363+Sejalkatre@users.noreply.github.com>
2026-04-08 22:39:50 +0530

    Update jenkins-install.sh

--- terraform/jenkins-install.sh
+++ terraform/jenkins-install.sh
@@ -10,7 +10,7 @@ apt-get install -y openjdk-11-jdk
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
     /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
-# Add Jenkins repository
+# Add Jenkins repository with signed-by option
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
     https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
     /etc/apt/sources.list.d/jenkins.list > /dev/null
