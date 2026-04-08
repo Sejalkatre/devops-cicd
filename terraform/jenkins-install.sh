082586c6a4f11eb3f044237817b4c259ebbb3de4
Sejal Katre <96768363+Sejalkatre@users.noreply.github.com>
2026-04-08 20:48:00 +0530

    Update jenkins-install.sh

--- terraform/jenkins-install.sh
+++ terraform/jenkins-install.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
-yum update -y
-amazon-linux-extras install java-openjdk11 -y
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-yum install jenkins -y
-systemctl enable jenkins && systemctl start jenkins
-yum install docker git -y
-systemctl enable docker && systemctl start docker
+# Update system
+apt-get update -y
+apt-get upgrade -y
+
+# Install Java (required for Jenkins)
+apt-get install -y openjdk-11-jdk
+
+# Add Jenkins repository
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | apt-key add -
+sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+    /etc/apt/sources.list.d/jenkins.list'
+
+# Install Jenkins
+apt-get update -y
+apt-get install -y jenkins
+
+# Enable and start Jenkins service
+systemctl enable jenkins
+systemctl start jenkins
+
+# Install Docker and Git
+apt-get install -y docker.io git
+
+# Enable and start Docker
+systemctl enable docker
+systemctl start docker
+
+# Add Jenkins user to Docker group
 usermod -aG docker jenkins
