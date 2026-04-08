057f57205e006266fa36fd69fe07f824afe2cfbb
Sejal Katre <96768363+Sejalkatre@users.noreply.github.com>
2026-04-08 20:12:11 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,3 @@
 variable "region" { default = "us-west-2" }
-variable "ami_id" { default = "ami-0ec10929233384c7f" }
+variable "ami_id" { default = "ami-0d76b909de1a0595d" }
 variable "key_name" { description = "devops-key" }
