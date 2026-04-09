24f7d9efaeefd2191eb715282d4b38bc9f134921
Sejal Katre <96768363+Sejalkatre@users.noreply.github.com>
2026-04-09 19:42:05 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.0"   # ensure AWS provider 5.x
     }
   }
 }
@@ -13,140 +13,60 @@ provider "aws" {
   region = var.region
 }
 
-############################
-# VPC
-############################
+# VPC Module
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.2"
+  version = "5.1.2"   # pin stable version
 
-  name    = "cicd-vpc"
+  name    = "devops-vpc"
   cidr    = "10.0.0.0/16"
 
   azs             = ["${var.region}a", "${var.region}b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
 
   enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_vpn_gateway = false
 }
 
-############################
-# Security Group
-############################
-resource "aws_security_group" "main_sg" {
-  name   = "main-sg"
-  vpc_id = module.vpc.vpc_id
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Jenkins UI"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "ArgoCD"
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description = "Allow all outbound"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-############################
-# EKS Cluster
-############################
+# EKS Module
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.4"
+  version = "20.8.4"   # pin stable version
 
-  cluster_name    = "simple-eks-cluster"
-  cluster_version = "1.30"   # <-- use a supported version in your region
+  cluster_name    = "devops-cluster"
+  cluster_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
   cluster_endpoint_public_access = true
 
+  # Add a managed node group with 1 worker node
   eks_managed_node_groups = {
     default = {
-      instance_types = ["t3.small"]
-      desired_size   = 1
-      min_size       = 1
-      max_size       = 1
+      desired_size = 1
+      min_size     = 1
+      max_size     = 2
+
+      instance_types = ["t3.medium"]
+      capacity_type  = "ON_DEMAND"
     }
   }
 }
 
-############################
-# Jenkins EC2 Instance
-############################
+# Jenkins EC2 Instance (Ubuntu Server)
 resource "aws_instance" "jenkins" {
-  ami           = var.ami_id   # Ubuntu AMI ID
-  instance_type = "t3.small"
-
+  ami                         = var.ami_id   # Ubuntu AMI ID for your region
+  instance_type               = "t3.medium"
   subnet_id                   = module.vpc.public_subnets[0]
-  key_name                    = var.key_name
   associate_public_ip_address = true
+  key_name                    = var.key_name
 
-  vpc_security_group_ids = [aws_security_group.main_sg.id]
+  # Install Jenkins, Docker, OpenJDK automatically
+  user_data = file("${path.module}/jenkins-install.sh")
 
   tags = {
     Name = "Jenkins-Server"
   }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt update -y
-              sudo apt install -y docker.io git unzip curl
-              sudo systemctl start docker
-              sudo usermod -aG docker ubuntu
-
-              # Install Jenkins
-              wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-              sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-              sudo apt update
-              sudo apt install -y openjdk-11-jdk jenkins
-              sudo systemctl enable jenkins
-              sudo systemctl start jenkins
-
-              # Install AWS CLI v2
-              curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-              unzip awscliv2.zip
-              sudo ./aws/install
-  EOF
 }
