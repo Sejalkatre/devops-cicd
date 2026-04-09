a9f2966eea5453ecaf45f754cdb2dfcf2e9d28dc
Sejal Katre <96768363+Sejalkatre@users.noreply.github.com>
2026-04-09 08:21:13 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,9 +50,9 @@ module "eks" {
   eks_managed_node_groups = {
     default = {
       min_size      = 2
-      max_size      = 4
+      max_size      = 2
       desired_size  = 2
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.small"]
       capacity_type  = "ON_DEMAND"
     }
   }
