4dadfb45d3bd61f0ef9b8e37752da09e2a05c8f5
Sejal Katre <96768363+Sejalkatre@users.noreply.github.com>
2026-04-09 21:39:48 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"   # ensure AWS provider 5.x
+      version = ">= 5.0, < 6.0"
     }
   }
 }
