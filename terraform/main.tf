94a5cc6e27ebd288fbfb3e4dccdf622af389bac9
Sejal Katre <96768363+Sejalkatre@users.noreply.github.com>
2026-04-09 21:44:25 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0, < 6.0"
+      version = "~> 5.0"   # locks you to AWS provider 5.x (e.g. 5.100.0)
     }
   }
 }
