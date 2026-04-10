db99c3281cb2821605137b0c21a23945ad81c64a
Sejal Katre <96768363+Sejalkatre@users.noreply.github.com>
2026-04-10 10:16:49 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,2 +1,15 @@
-output "jenkins_public_ip" { value = aws_instance.jenkins.public_ip }
-output "eks_cluster_name" { value = module.eks.cluster_name }
+output "jenkins_public_ip" {
+  value = aws_instance.jenkins.public_ip
+}
+
+output "eks_cluster_name" {
+  value = module.eks.cluster.name
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster.endpoint
+}
+
+output "eks_cluster_version" {
+  value = module.eks.cluster.version
+}
