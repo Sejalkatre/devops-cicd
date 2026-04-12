4aa3cb1711ba84f236a8c4d506267ca31f7c869c
Sejal Katre <96768363+Sejalkatre@users.noreply.github.com>
2026-04-12 07:32:46 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,17 @@
+# Public IP of Jenkins EC2
 output "jenkins_public_ip" {
   value = aws_instance.jenkins.public_ip
 }
 
+# EKS Cluster Outputs (v20.x schema)
 output "eks_cluster_name" {
-  value = module.eks.cluster.name
+  value = module.eks.cluster_id
 }
 
 output "eks_cluster_endpoint" {
-  value = module.eks.cluster.endpoint
+  value = module.eks.cluster_endpoint
 }
 
 output "eks_cluster_version" {
-  value = module.eks.cluster.version
+  value = module.eks.cluster_version
 }
